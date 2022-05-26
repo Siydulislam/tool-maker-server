ca29c2847976356e6e2008e9aaf3454b05d5ff1a
Siydul Islam <siydulislam333@gmail.com>
2022-05-26 20:09:33 +0600

    delete individual product

--- index.js
+++ index.js
@@ -102,6 +102,14 @@ async function run() {
             }
         });
 
+        // Delete an Order
+        app.delete('/order/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const deleteOrder = await orderCollection.deleteOne(filter);
+            res.send(deleteOrder);
+        })
+
         // Add a review
         app.post('/review', async (req, res) => {
             const review = req.body;
